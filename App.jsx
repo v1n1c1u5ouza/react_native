bc0ebbadf46094b4518f72862fecca0b0dade645
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2023-02-23 15:05:57 -0300

    Uploading files

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { View, Text } from "react-native";
+
+function App() {
+  return(
+    <View>
+      <Text>
+        Olá mundo 
+      </Text>
+    </View>
+  )
+}
+
+export default App
